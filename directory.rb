ddd3b16bfb5e4fd430e5c8bb064cc4fa432424d6
Patrick Sawyer <patrick_sawyer@hotmail.co.uk>
2020-04-02 14:27:43 +0100

    after exercises

--- directory.rb
+++ directory.rb
@@ -5,12 +5,35 @@ def input_students
   puts "To finish just hit return twice"
   # create empty array
   students = []
+  months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+  	]
   # get the first name
-  name = gets.chomp
+  name = gets[0...-1].capitalize
   while !name.empty? do
-    students << {name: name, cohort: :november}
+  	puts "Cohort?"
+  	cohort = gets[0...-1].capitalize
+    while !months.include? cohort
+      puts "Typo. Try again. Cohort?"
+  	  cohort = gets[0...-1].capitalize
+    end
+  	if cohort.length == 0
+  	  cohort = "WHATEVER"
+  	end
+    students << {name: name, cohort: cohort.to_sym}
     puts "Now we have #{students.count} students"
-    name = gets.chomp
+    name = gets[0...-1].capitalize
   end
   students
 end
@@ -23,13 +46,60 @@ def print_header
 end
 
 def print(names)
-  names.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
+
+# using each
+
+#  names.each_with_index do |student, index|
+#  	if student[0] == "D" && student.length < 12
+#      puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+#    end
+#  end
+
+# using while
+
+#  name_index = 0
+#  while name_index < names.length do
+#    puts ((name_index + 1).to_s + ". " + names[name_index][:name] + " (" + names[name_index][:cohort].to_s + " cohort)").center(50)
+#    name_index += 1
+#  end
+
+# puts according to cohort
+
+  months = {
+      January: [],
+      February: [],
+      March: [],
+      April: [],
+      May: [],
+      June: [],
+      July: [],
+      August: [],
+      September: [],
+      October: [],
+      November: [],
+      December: []
+  }
+
+  names.each { |x|
+    months[x[:cohort]].push(x[:name])
+  }
+
+  months.each { |k,v|
+    if v != []
+  	  puts k.to_s + " cohort:"
+  	  puts v
+  	  puts "-------------"
+    end
+  }
+
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  word = "student"
+  if names.length() > 1
+    word << "s"
+  end
+  puts "Overall, we have #{names.count} great " + word
 end
 
 # calling methods
