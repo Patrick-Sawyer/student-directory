ff8b02550dc43c2032c20b4398b8f52481b159de
Patrick Sawyer <patrick_sawyer@hotmail.co.uk>
2020-04-02 13:00:17 +0100

    ADDED METHOD TO ADD NAMES

--- directory.rb
+++ directory.rb
@@ -1,20 +1,21 @@
-# students array
+# method to add students
 
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish just hit return twice"
+  # create empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
 
-# print
+# methods to print
 
 def print_header
   puts "The students of Villains Academy"
@@ -31,6 +32,9 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+# calling methods
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
