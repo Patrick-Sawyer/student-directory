0947d9e1b072ba7bfec9d6915e556808ca39a4e9
Patrick Sawyer <patrick_sawyer@hotmail.co.uk>
2020-04-07 11:42:50 +0100

    App completed

--- directory.rb
+++ directory.rb
@@ -1,22 +1,36 @@
-# open csv file
+# open csv fil
+
+require 'csv'
 
 @students = []
 
 def load_students(filename = "students.csv")
-  file = File.open(filename,"r")
-  file.readlines.each{ |line|
-    name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
-  }
-  file.close
+  if File.exists?(filename)
+    @students = []
+    file = File.open(filename,"r")
+    CSV.parse(file) do |row|
+      @students << {name: row[0], cohort: row[1].to_sym}
+    end
+    puts "Loaded #{@students.count} from #{filename}"
+    puts "-------------"
+  else
+    puts "No such file"
+    puts "-------------"
+  end
+end
+
+def add(name,cohort)
+  @students << {name: name, cohort: cohort.to_sym}
 end
 
 def try_load_students
   filename = ARGV.first
-  return if filename.nil?
+  if filename == nil
+    load_students
+    return
+  end
   if File.exists?(filename)
     load_students(filename)
-    puts "Loaded #{@students.count} from #{filename}"
   else
     puts "Sorry, #{filename} doesn't exist"
     exit
@@ -24,6 +38,10 @@ def try_load_students
 end
 
 # method to add students
+
+def input
+  STDIN.gets[0...-1].capitalize
+end
 
 def input_students
   puts "-------------"
@@ -46,36 +64,35 @@ def input_students
       "November",
       "December"
   	]
-  name = STDIN.gets[0...-1].capitalize
+
+  name = input
 
   # get name etc
 
   while !name.empty? do
   	puts "Cohort?"
-  	cohort = STDIN.gets[0...-1].capitalize
+  	cohort = input
     while !months.include? cohort
       puts "Typo. Try again. Cohort?"
-  	  cohort = STDIN.gets[0...-1].capitalize
+  	  cohort = input
     end
-  	if cohort.length == 0
-  	  cohort = "WHATEVER"
-  	end
 
     # push to array
 
-    @students << {name: name, cohort: cohort.to_sym}
+    add(name,cohort)
     word = "student"
     if @students.count != 1
       word << "s"
     end
-    puts "Now we have #{@students.count} #{word}"
-    name = STDIN.gets[0...-1].capitalize
+    puts "Now we have #{@students.count} #{word}. Type another or press Enter"
+    name = input
   end
 end
 
 # methods to print
 
 def print_header
+  puts "-------------"
   puts "The students of Villains Academy"
   puts "-------------"
 end
@@ -126,8 +143,9 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save to students.csv"
-  puts "4. Load students.csv"
+  puts "3. Save"
+  puts "4. Load"
+  puts "5. Print source code to screen"
   puts "9. Exit"
   puts "-------------"
 end
@@ -136,6 +154,10 @@ def show_students()
   print_header
   print_students_list
   print_footer
+end
+
+def source_code
+  puts File.open("directory.rb","r").read
 end
 
 def process(selection)
@@ -147,8 +169,13 @@ def process(selection)
     when "3"
       save_students
     when "4"
-      load_students
+      puts "Which file would you like to load?"
+      filename = STDIN.gets.chomp
+      load_students(filename)
+    when "5"
+      source_code
     when "9"
+      puts "-------------"
       exit
     else
       puts "I don't know what you mean, try again"      
@@ -165,13 +192,15 @@ end
 # saving
 
 def save_students
-  file = File.open("students.csv", "w")
-  @students.each { |student|
-    student_data = [student[:name]],[student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  }
-  file.close
+  puts "Which file would you like to write on?"
+  filename = STDIN.gets.chomp
+  CSV.open(filename, "wb") do |csv|
+  	@students.each { |student|
+  		csv << [student[:name],student[:cohort]]
+  	}
+  end
+  puts "Save successful"
+  puts "-------------"
 end
 
 # call menu method
